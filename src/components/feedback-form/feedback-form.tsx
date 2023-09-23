4161456fc0d59b224cd9ca242ceedae045106237
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-23 01:45:45 +0100

    feat: spliting the code

--- src/components/feedback-form/feedback-form.tsx
+++ src/components/feedback-form/feedback-form.tsx
@@ -1,8 +1,8 @@
 import { FeedbackProvider } from "../../contexts/feedback-context";
-import { FeedbackContainer } from "../feedback-container";
-import { FeedbackHeader } from "../feedback-header";
-import { FeedbackBody } from "../feedback-body";
-import { FeedbackFooter } from "../feedback-footer";
+import { FeedbackContainer } from "./feedback-container";
+import { FeedbackHeader } from "./feedback-header";
+import { FeedbackBody } from "./feedback-body";
+import { FeedbackFooter } from "./feedback-footer";
 import { Divisor } from "../shared";
 
 export const FeedbackForm = () => {
