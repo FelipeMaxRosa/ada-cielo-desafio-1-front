6310eda3b3a59fb8cd29528c0fd3df1b6c0aa202
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-24 22:39:35 +0100

    feat: api integration

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
+
+import { API_URL } from '../constants/services-url';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: API_URL,
 });
 
 export default api;
--- src/services/feedback-service.ts
+++ src/services/feedback-service.ts
@@ -1,11 +1,7 @@
-import api from "./api";
-import { URL } from "./constants";
-import { Feedback } from "../models/interfaces";
+import api from './api';
+import { URL } from './constants';
+import { Feedback, FeedbackData } from '../models/interfaces';
 
-interface FeedbackData {
-  feedbackMessage: string;
-  feedbackType: string;
-}
 function submitFeedback(feedbackData: FeedbackData) {
   return api.post<Feedback[]>(URL.FEEDBACK, feedbackData);
 }
