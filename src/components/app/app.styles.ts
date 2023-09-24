ec47efcbf23a50a71ee77214ace340c94f1e2d0c
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-24 16:33:35 +0100

    feat: feedbacks table

--- src/components/app/app.styles.ts
+++ src/components/app/app.styles.ts
@@ -1,5 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
+import { devices } from '../../styles/constants/breakpoints';
 
 export const AppContainer = styled.main`
-  padding: 80px;
+  display: flex;
+  gap: 40px;
+
+  flex-direction: column;
+  padding: 16px;
+
+  @media ${devices.lg} {
+    flex-direction: row;
+  }
+  /* flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 80px; */
 `;
--- src/components/feedback-form/feedback-container/feedback-container.styles.ts
+++ src/components/feedback-form/feedback-container/feedback-container.styles.ts
@@ -1,10 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 460px;
+  width: 100%;
+  min-width: 260px;
+  max-width: 400px;
 
   padding: 36px 24px;
-  margin: 0 auto;
   background: #ffffff;
   border-radius: 24px;
   box-shadow: 2px 2px 5px black;
